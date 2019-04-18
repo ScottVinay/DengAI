238dce6fe7f8177956a8bead50eed436eaeebf31
lbitton93 <36272279+lbitton93@users.noreply.github.com>
2019-04-18 13:24:27 +0100

    Update Lukes_funcs.py

--- project_files/Lukes_funcs.py
+++ project_files/Lukes_funcs.py
@@ -1,24 +1,22 @@
-def drop_columns(s, drp_lst = None,):
-  
+    def drop_columns(s, col_lst = None,):
   
   " | --------------------------------------------------------------------------- | "
   " | This function is passed a dataframe s and a list of column names to drop.   | "
   " | It will alert the user if an invalid column key is passed and exit.         | "
   " | --------------------------------------------------------------------------- | "
-  
-  
-  if len(drp_lst) != 0:
+    
+  if len(col_lst) != 0:
+    
     try:
-        s.drop(drop_list, axis=1,inplace=True)        
+        s.drop(col_lst, axis=1,inplace=True)      
     except(KeyError):
-      
           plural = 0
-          for word in drp_lst:
+          for word in col_lst:
             if word not in s.columns:
               plural = plural + 1
               print(word,", ", end="")
-              
-          if plural == 2:
+            
+          if plural <2:
             print("is not a column name within dataframe, please remove it from the drop list")
           else:
             print("are not a column names within dataframe, please remove them from the drop list")
